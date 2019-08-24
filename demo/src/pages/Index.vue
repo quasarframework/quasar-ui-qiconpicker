79ce0bfcd114149596b1f9da22e17da246b8a636
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-24 11:12:10 -0600

    feat(docs): add component api to docs

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -1,15 +1,27 @@
 <template>
   <hero>
     <q-markdown :src="markdown" toc @data="onToc" />
+    <component-api
+      title="QIconPicker API"
+      :json="json"
+    />
+    <q-markdown>
+# Donate
+If you appreciate the work that went into this App Extension, please consider [donating to Quasar](https://donate.quasar.dev).
+
+---
+This page created with [QMarkdown](https://quasarframework.github.io/app-extension-qmarkdown), another great Quasar App Extension.
+    </q-markdown>
+    <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+      <q-btn fab icon="keyboard_arrow_up" color="primary" />
+    </q-page-scroller>
   </hero>
 </template>
-
-<style>
-</style>
 
 <script>
 import Hero from '../components/Hero'
 import markdown from '../markdown/iconpicker.md'
+import json from '@quasar/quasar-app-extension-qiconpicker/src/component/QIconPicker.json'
 
 export default {
   name: 'PageIndex',
@@ -20,7 +32,8 @@ export default {
 
   data () {
     return {
-      markdown: markdown
+      markdown: markdown,
+      json: json
     }
   },
 
@@ -39,6 +52,10 @@ export default {
 
   methods: {
     onToc (toc) {
+      // add the manual ones
+      toc.push({ id: 'QIconPicker-API', label: 'QIconPicker API', level: 1, children: Array(0) })
+      toc.push({ id: 'Donate', label: 'Donate', level: 1, children: Array(0) })
+
       this.toc = toc
     }
   }
