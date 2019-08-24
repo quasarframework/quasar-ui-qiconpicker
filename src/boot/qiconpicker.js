96067394fdbb9f50e2879377a358a8ab81980f8c
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-08-24 11:16:02 -0600

    chore: add colorize tanspile, etc

--- src/boot/qiconpicker.js
+++ src/boot/qiconpicker.js
@@ -1,5 +1,7 @@
 import QIconPicker from '@quasar/quasar-app-extension-qiconpicker/src/component/QIconPicker'
+import { Colorize } from 'quasar-mixin-colorize'
 
 export default ({ Vue }) => {
   Vue.component('q-icon-picker', QIconPicker)
+  Vue.component(Colorize)
 }
