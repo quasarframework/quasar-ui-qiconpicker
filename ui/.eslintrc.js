b9fb219ed2e11cb29512fe83a9707007793b8d3b
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-08 09:59:01 -0700

    chore(demo): misc updates

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -47,8 +47,9 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'prefer-promise-reject-errors': 'off',
 
+    'no-void': 'off',
     // allow console.log during development only
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
