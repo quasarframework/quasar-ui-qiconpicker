2bd5d0856833ce0389ae0c09ccf4684eedbf6bd8
Jeff Galbraith <galbraith64@gmail.com>
2021-07-29 08:28:30 -0600

    chore(dev): moved to root folder

---
+++ dev/src/pages/Size.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-lg flex flex-center">
+      <div style="width: 700px;">
+        <q-icon-picker
+          v-model="data.value"
+          icon-set="material-icons"
+          font-size="30px"
+          v-model:model-pagination="data.pagination"
+          style="height: 360px;"
+        ></q-icon-picker>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref, watch } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const data = ref({
+      value: '',
+      pagination: {
+        itemsPerPage: 32,
+        page: 0
+      }
+    })
+
+    return {
+      data
+    }
+  }
+})
+</script>
