cc6e026187ce671c55a7046bda0d470229da76ef
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2023-09-30 09:36:07 +0100

    Update QIconPicker.js: Handle synchronous vite imports.

--- ui/src/components/QIconPicker.js
+++ ui/src/components/QIconPicker.js
@@ -12,7 +12,6 @@ import {
   h,
   nextTick,
   onMounted,
-  reactive,
   ref,
   watch,
 } from "vue";
@@ -87,264 +86,6 @@ const direction = {
   PREV: "prev",
 };
 
-/**
- * Pagination
- */
-function useIconPickerPagination (data, props, emit, computedFilteredIcons) {
-  function fixPagination (p) {
-    if (p.page < 1) {
-      p.page = 1;
-    }
-    if (p.itemsPerPage === void 0 || p.itemsPerPage < 1) {
-      p.itemsPerPage = 0; // all
-    }
-    return p;
-  }
-
-  // returns true if the pagination is the same,
-  // otherwise returns false if it has changed
-  function samePagination (oldPag, newPag) {
-    for (const prop in newPag) {
-      if (newPag[prop] !== oldPag[prop]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  const computedPagination = computed(() => {
-    return fixPagination({
-      ...data.innerPagination,
-      ...props.modelPagination,
-    });
-  });
-
-  const computedPagesNumber = computed(() => {
-    return computedPagination.value.itemsPerPage === 0
-      ? 1
-      : Math.max(
-        1,
-        Math.ceil(
-          computedFilteredIcons.value.length /
-          computedPagination.value.itemsPerPage,
-        ),
-      );
-  });
-
-  function setPagination (val) {
-    const newPagination = fixPagination({
-      ...computedPagination.value,
-      ...val,
-    });
-
-    if (!samePagination(data.innerPagination, newPagination)) {
-      if (props.modelPagination) {
-        emit("update:model-pagination", newPagination);
-      }
-      data.innerPagination = newPagination;
-    }
-  }
-
-  function updatePagination () {
-    if (props.modelPagination !== void 0) {
-      setPagination({
-        total: computedFilteredIcons.value.length,
-        totalPages: computedPagesNumber.value,
-      });
-    }
-  }
-
-  return {
-    samePagination,
-    computedPagination,
-    setPagination,
-    updatePagination,
-    computedPagesNumber,
-  };
-}
-
-/**
- * Icons
- */
-function useIconPickerIcons (
-  data,
-  props,
-  computedFirstItemIndex,
-  computedLastItemIndex,
-) {
-  function loadIconSet (iconSet) {
-    data.iconsList = [];
-    if (iconSet) {
-      // detect if UMD version is installed
-      if (window.QIconPicker) {
-        const name = iconSet.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
-        if (window.QIconPicker.iconSet && window.QIconPicker.iconSet[name]) {
-          data.iconsList = window.QIconPicker.iconSet[name].icons;
-        } else {
-          console.error(`QIconPicker: no icon set loaded called ${iconSet}`);
-          console.error(
-            "Be sure to load the UMD version of the icon set in a script tag before using QIconPicker UMD version",
-          );
-        }
-        console.info(`Loaded ${data.iconsList.length} icons.`);
-      } else {
-        try {
-          // If vite
-          if (typeof import.meta !== 'undefined' && import.meta.env !== undefined) {
-            viteImporter(iconSet)
-            // Else if webpack
-          } else {
-            data.iconsList = require(
-              `@quasar/quasar-ui-qiconpicker/src/components/icon-set/${iconSet}.js`,
-            ).default.icons;
-            console.info(`Loaded ${data.iconsList.length} icons.`);
-          }
-        } catch (e) {
-          console.error(
-            `QIconPicker: cannot find icon set found called ${iconSet}`,
-          );
-        }
-      }
-    }
-  }
-
-  const viteImporter = async (iconSet) => {
-    const set = await import(`./icon-set/${iconSet}.js`)
-    data.iconsList = set.default.icons
-    console.info(`Loaded ${data.iconsList.length} icons.`);
-  }
-
-  const computedDisplayedIcons = computed(() => {
-    let icons = [];
-    if (data.iconsList) {
-      icons = computedFilteredIcons.value;
-
-      // should the icons be paged?
-      if (props.modelPagination && props.modelPagination.itemsPerPage !== 0) {
-        icons = icons.slice(
-          computedFirstItemIndex.value,
-          computedLastItemIndex.value,
-        );
-      }
-    }
-    return icons;
-  });
-
-  const computedFilteredIcons = computed(() => {
-    let icons = data.iconsList;
-    if (icons) {
-      if (
-        props.tags !== void 0 &&
-        props.tags !== "" &&
-        props.tags !== null &&
-        props.tags.length > 0
-      ) {
-        icons = icons.filter((icon) => {
-          return icon.tags.filter((tag) => props.tags.includes(tag)).length > 0;
-        });
-      }
-      if (
-        props.filter !== void 0 &&
-        props.filter !== "" &&
-        props.filter !== null
-      ) {
-        icons = icons.filter((icon) => icon.name.includes(props.filter));
-      }
-    }
-    return icons;
-  });
-
-  function categories () {
-    const t = [];
-    data.iconsList.forEach((icon) => {
-      const tags = icon.tags;
-      if (tags && tags.length > 0) {
-        tags.forEach((tag) => {
-          if (t.includes(tag) !== true) {
-            t.push(tag);
-          }
-        });
-      }
-    });
-    t.sort();
-    data.categories = t;
-    return true;
-  }
-
-  return {
-    loadIconSet,
-    computedDisplayedIcons,
-    computedFilteredIcons,
-    categories,
-  };
-}
-
-/**
- * Exposes api functions
- */
-function exposeIconPickerApi (
-  data,
-  expose,
-  computedPagination,
-  setPagination,
-  computedFirstItemIndex,
-  computedLastItemIndex,
-  computedFilteredIcons,
-  computedPagesNumber,
-) {
-  // goes to previous page
-  const prevPage = () => {
-    const { page } = computedPagination.value;
-    if (page > 1) {
-      setPagination({ page: page - 1 });
-      data.direction = direction.PREV;
-    }
-  };
-
-  // goes to next page
-  const nextPage = () => {
-    const { page, itemsPerPage } = computedPagination.value;
-    if (
-      computedLastItemIndex.value > 0 &&
-      page * itemsPerPage < computedFilteredIcons.value.length
-    ) {
-      setPagination({ page: page + 1 });
-      data.direction = direction.NEXT;
-    }
-  };
-
-  // goes to last page
-  const lastPage = () => {
-    setPagination({ page: computedPagesNumber.value });
-  };
-
-  // goes to first page
-  const firstPage = () => {
-    setPagination({ page: 0 });
-  };
-
-  // checks if we are on the last page
-  const isLastPage = computed(() => {
-    return computedLastItemIndex.value === 0
-      ? true
-      : computedPagination.value.page >= computedPagesNumber.value;
-  });
-
-  // checks if we are on the first page
-  const isFirstPage = computed(() => {
-    return computedPagination.value.page === 1;
-  });
-
-  expose({
-    prevPage,
-    nextPage,
-    lastPage,
-    firstPage,
-    isLastPage,
-    isFirstPage,
-  });
-}
-
 export default defineComponent({
   name: "QIconPicker",
 
@@ -356,7 +97,8 @@ export default defineComponent({
 
   setup (props, { slots, emit, expose }) {
     const scrollAreaRef = ref(null);
-    const data = reactive({
+    const data = ref({
+      viteLoaded: false,
       iconsList: [],
       innerPagination: {
         page: 1,
@@ -381,17 +123,209 @@ export default defineComponent({
       return page * itemsPerPage;
     });
 
+    /**
+     * Icons
+     */
+    const useIconPickerIcons = (
+      computedFirstItemIndex,
+      computedLastItemIndex,
+    ) => {
+      function loadIconSet (iconSet) {
+        data.value.iconsList = [];
+        if (iconSet) {
+          // detect if UMD version is installed
+          if (window.QIconPicker) {
+            const name = iconSet.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+            if (window.QIconPicker.iconSet && window.QIconPicker.iconSet[name]) {
+              data.value.iconsList = window.QIconPicker.iconSet[name].icons;
+            } else {
+              console.error(`QIconPicker: no icon set loaded called ${iconSet}`);
+              console.error(
+                "Be sure to load the UMD version of the icon set in a script tag before using QIconPicker UMD version",
+              );
+            }
+            console.info(`Loaded ${data.value.iconsList.length} icons.`);
+          } else {
+            try {
+              // If vite
+              if (typeof import.meta !== 'undefined' && import.meta.env !== undefined) {
+                viteImporter(iconSet)
+                // Else if webpack
+              } else {
+                data.value.iconsList = require(
+                  `@quasar/quasar-ui-qiconpicker/src/components/icon-set/${iconSet}.js`,
+                ).default.icons;
+                console.info(`Loaded ${data.value.iconsList.length} icons.`);
+              }
+            } catch (e) {
+              console.error(
+                `QIconPicker: cannot find icon set found called ${iconSet}`,
+              );
+            }
+          }
+        }
+      }
+
+      const viteImporter = async (iconSet) => {
+        console.info('Vite support detected');
+        const set = await import(`./icon-set/${iconSet}.js`)
+        data.value.iconsList = set.default.icons
+        data.value.viteLoaded = true
+        console.info(`Loaded ${data.value.iconsList.length} icons.`);
+      }
+
+      const computedDisplayedIcons = computed(() => {
+        let icons = [];
+        if (data.value.iconsList) {
+          icons = computedFilteredIcons.value;
+
+          // should the icons be paged?
+          if (props.modelPagination && props.modelPagination.itemsPerPage !== 0) {
+            icons = icons.slice(
+              computedFirstItemIndex.value,
+              computedLastItemIndex.value,
+            );
+          }
+        }
+        return icons;
+      });
+
+      const computedFilteredIcons = computed(() => {
+        let icons = data.value.iconsList;
+        if (icons) {
+          if (
+            props.tags !== void 0 &&
+            props.tags !== "" &&
+            props.tags !== null &&
+            props.tags.length > 0
+          ) {
+            icons = icons.filter((icon) => {
+              return icon.tags.filter((tag) => props.tags.includes(tag)).length > 0;
+            });
+          }
+          if (
+            props.filter !== void 0 &&
+            props.filter !== "" &&
+            props.filter !== null
+          ) {
+            icons = icons.filter((icon) => icon.name.includes(props.filter));
+          }
+        }
+        return icons;
+      });
+
+      function categories () {
+        const t = [];
+        data.value.iconsList.forEach((icon) => {
+          const tags = icon.tags;
+          if (tags && tags.length > 0) {
+            tags.forEach((tag) => {
+              if (t.includes(tag) !== true) {
+                t.push(tag);
+              }
+            });
+          }
+        });
+        t.sort();
+        data.value.categories = t;
+        return true;
+      }
+
+      return {
+        loadIconSet,
+        computedDisplayedIcons,
+        computedFilteredIcons,
+        categories,
+      };
+    }
+
     const {
       loadIconSet,
       computedDisplayedIcons,
       computedFilteredIcons,
       categories,
     } = useIconPickerIcons(
-      data,
-      props,
       computedFirstItemIndex,
       computedLastItemIndex,
     );
+
+    /**
+     * Pagination
+     */
+    function useIconPickerPagination () {
+      function fixPagination (p) {
+        if (p.page < 1) {
+          p.page = 1;
+        }
+        if (p.itemsPerPage === void 0 || p.itemsPerPage < 1) {
+          p.itemsPerPage = 0; // all
+        }
+        return p;
+      }
+
+      // returns true if the pagination is the same,
+      // otherwise returns false if it has changed
+      function samePagination (oldPag, newPag) {
+        for (const prop in newPag) {
+          if (newPag[prop] !== oldPag[prop]) {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      const computedPagination = computed(() => {
+        return fixPagination({
+          ...data.value.innerPagination,
+          ...props.modelPagination,
+        });
+      });
+
+      const computedPagesNumber = computed(() => {
+        return computedPagination.value.itemsPerPage === 0
+          ? 1
+          : Math.max(
+            1,
+            Math.ceil(
+              computedFilteredIcons.value.length /
+              computedPagination.value.itemsPerPage,
+            ),
+          );
+      });
+
+      function setPagination (val) {
+        const newPagination = fixPagination({
+          ...computedPagination.value,
+          ...val,
+        });
+
+        if (!samePagination(data.value.innerPagination, newPagination)) {
+          if (props.modelPagination) {
+            emit("update:model-pagination", newPagination);
+          }
+          data.value.innerPagination = newPagination;
+        }
+      }
+
+      function updatePagination () {
+        if (props.modelPagination !== void 0) {
+          setPagination({
+            total: computedFilteredIcons.value.length,
+            totalPages: computedPagesNumber.value,
+          });
+        }
+      }
+
+      return {
+        samePagination,
+        computedPagination,
+        setPagination,
+        updatePagination,
+        computedPagesNumber,
+      };
+    }
+
+    // ==========
 
     const {
       samePagination,
@@ -399,12 +333,73 @@ export default defineComponent({
       setPagination,
       updatePagination,
       computedPagesNumber,
-    } = useIconPickerPagination(data, props, emit, computedFilteredIcons);
+    } = useIconPickerPagination();
+
+
+    /**
+     * Exposes api functions
+     */
+    const exposeIconPickerApi = (
+      setPagination,
+      computedFirstItemIndex,
+      computedLastItemIndex,
+      computedFilteredIcons,
+      computedPagesNumber,
+    ) => {
+      // goes to previous page
+      const prevPage = () => {
+        const { page } = computedPagination.value;
+        if (page > 1) {
+          setPagination({ page: page - 1 });
+          data.value.direction = direction.PREV;
+        }
+      };
+
+      // goes to next page
+      const nextPage = () => {
+        const { page, itemsPerPage } = computedPagination.value;
+        if (
+          computedLastItemIndex.value > 0 &&
+          page * itemsPerPage < computedFilteredIcons.value.length
+        ) {
+          setPagination({ page: page + 1 });
+          data.value.direction = direction.NEXT;
+        }
+      };
+
+      // goes to last page
+      const lastPage = () => {
+        setPagination({ page: computedPagesNumber.value });
+      };
+
+      // goes to first page
+      const firstPage = () => {
+        setPagination({ page: 0 });
+      };
+
+      // checks if we are on the last page
+      const isLastPage = computed(() => {
+        return computedLastItemIndex.value === 0
+          ? true
+          : computedPagination.value.page >= computedPagesNumber.value;
+      });
+
+      // checks if we are on the first page
+      const isFirstPage = computed(() => {
+        return computedPagination.value.page === 1;
+      });
+
+      expose({
+        prevPage,
+        nextPage,
+        lastPage,
+        firstPage,
+        isLastPage,
+        isFirstPage,
+      });
+    }
 
     exposeIconPickerApi(
-      data,
-      expose,
-      computedPagination,
       setPagination,
       computedFirstItemIndex,
       computedLastItemIndex,
@@ -416,10 +411,16 @@ export default defineComponent({
       if (props.iconSet) {
         loadIconSet(props.iconSet);
       } else if (props.icons !== void 0 && props.icons.length > 0) {
-        data.iconsList = props.icons;
+        data.value.iconsList = props.icons;
       }
       updatePagination();
     });
+
+    watch(() => data.value.viteLoaded, (viteLoaded) => {
+      if (viteLoaded === true) {
+        updatePagination();
+      }
+    })
 
     watch(
       () => props.iconSet,
@@ -443,7 +444,7 @@ export default defineComponent({
       () => props.icons,
       () => {
         if (props.icons !== void 0 && props.icons.length > 0) {
-          data.iconsList = props.icons;
+          data.value.iconsList = props.icons;
         }
         updatePagination();
         nextTick(() => {
@@ -519,9 +520,9 @@ export default defineComponent({
             "onUpdate:modelValue": (value) => {
               if (props.animated) {
                 if (value > page) {
-                  data.direction = direction.NEXT;
+                  data.value.direction = direction.NEXT;
                 } else {
-                  data.direction = direction.PREV;
+                  data.value.direction = direction.PREV;
                 }
               }
               setPagination({ page: value });
@@ -597,7 +598,7 @@ export default defineComponent({
         if (props.animated === true) {
           const transition =
             "q-transition--" +
-            (data.direction === "prev"
+            (data.value.direction === "prev"
               ? props.transitionPrev
               : props.transitionNext);
           return () =>
@@ -620,8 +621,8 @@ export default defineComponent({
           {
             ref: scrollAreaRef,
             style: {
-              width: data.width + "px",
-              height: data.height + "px",
+              width: data.value.width + "px",
+              height: data.value.height + "px",
             },
           },
           renderContainer(),
@@ -638,8 +639,8 @@ export default defineComponent({
             renderScrollArea(),
             h(QResizeObserver, {
               onResize: (size) => {
-                data.width = size.width;
-                data.height = size.height;
+                data.value.width = size.width;
+                data.value.height = size.height;
               },
             }),
           ],
@@ -660,7 +661,7 @@ export default defineComponent({
 
       nextTick(() => {
         categories();
-        emit("update:tags", data.categories);
+        emit("update:tags", data.value.categories);
       }).catch((e) => console.error(e));
 
       return picker;
